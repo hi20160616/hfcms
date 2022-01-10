14aab119c76c26193006794d7af6e1dfd72b79cc
hi20160616 <hi20160616@163.com>
2022-01-10 18:01:46 +0800

    http server pass test.

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"golang.org/x/sync/errgroup"
 
 	"github.com/golang/glog"
 	"github.com/hi20160616/hfcms/configs"
 	"github.com/hi20160616/hfcms/internal/server"
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -18,8 +24,32 @@ func main() {
 	defer cancel()
 	g, ctx := errgroup.WithContext(ctx)
 	cfg := configs.NewConfig("hfcms")
-	glog.Info("http server start on %s", cfg.Web.Addr)
+
+	// Http server
 	g.Go(func() error {
-		return server.Run(ctx, cfg.Web.Addr, g)
+		return server.Run(ctx, cfg.Web.Addr)
 	})
+
+	// Graceful stop
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	g.Go(func() error {
+		select {
+		case sig := <-sigs:
+			fmt.Println()
+			glog.Infof("signal caught: %s, ready to quit...", sig.String())
+			cancel()
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+		return nil
+	})
+
+	if err := g.Wait(); err != nil {
+		if !errors.Is(err, context.Canceled) {
+			glog.Errorf("not canceled by context: %s", err)
+		} else {
+			glog.Info(err)
+		}
+	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -2,18 +2,13 @@ package server
 
 import (
 	"context"
-	"errors"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/golang/glog"
 	"github.com/hi20160616/hfcms/internal/server/handler"
-	"golang.org/x/sync/errgroup"
 )
 
-func Run(ctx context.Context, addr string, g *errgroup.Group) error {
+func Run(ctx context.Context, addr string) error {
 	defer func() {
 		if err := recover(); err != nil {
 			e := err.(error)
@@ -25,42 +20,16 @@ func Run(ctx context.Context, addr string, g *errgroup.Group) error {
 		Addr:    addr,
 		Handler: handler.GetHandler(),
 	}
-	g.Go(func() error {
-		if err := s.ListenAndServe(); err != http.ErrServerClosed {
-			return err
-		}
-		return ctx.Err()
-	})
-	g.Go(func() error {
+	go func() error {
 		<-ctx.Done()
-		glog.Info("Shutdown http server: %s", addr)
+		glog.Infof("Shutdown http server: %s", addr)
 		return s.Shutdown(ctx)
-	})
-	gracefulStop(ctx, g)
-	return ctx.Err()
-}
+	}()
 
-func gracefulStop(ctx context.Context, g *errgroup.Group) {
-	// Graceful stop
-	ctx, cancel := context.WithCancel(ctx)
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
-	g.Go(func() error {
-		select {
-		case sig := <-sigs:
-			glog.Errorf("signal caught: %s, ready to quit...", sig.String())
-			cancel()
-		case <-ctx.Done():
-			return ctx.Err()
-		}
-		return nil
-	})
-
-	if err := g.Wait(); err != nil {
-		if !errors.Is(err, context.Canceled) {
-			glog.Errorf("not canceled by context: %s", err)
-		} else {
-			glog.Error(err)
-		}
+	glog.Infof("http server start on %s", addr)
+	if err := s.ListenAndServe(); err != http.ErrServerClosed {
+		return err
 	}
+
+	return ctx.Err()
 }
